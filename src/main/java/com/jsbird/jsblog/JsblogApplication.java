462adda2351fe1251781053bda47a587eba0d51d
suRin01 <soullog77@gmail.com>
2022-05-19 03:11:37 +0900

    init project capstone design 2022 Front - React Back - Spring boot

---
+++ src/main/java/com/jsbird/jsblog/JsblogApplication.java
@@ -0,0 +1,17 @@
+package com.jsbird.jsblog;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class JsblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsblogApplication.class, args);
+        log.info("Start server");
+    }
+
+}
---
+++ src/test/java/com/jsbird/jsblog/JsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsbird.jsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
